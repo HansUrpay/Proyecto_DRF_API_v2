9b8ee45505907cf06c6b752c454d77b200d0132d
HansUrpay <hansurpayh@gmail.com>
2022-12-28 22:22:27 -0500

    rename BBDD

--- payments_2/models.py
+++ payments_2/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from services_2.models import Services
 
 # Create your models here.
-class Payment_user(models.Model):
+class PaymentUser(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -14,8 +14,8 @@ class Payment_user(models.Model):
     class Meta:
         db_table = 'payment_user'
 
-class Expired_payments(models.Model):
-    payment_user_id = models.ForeignKey(Payment_user, on_delete=models.CASCADE)
+class ExpiredPayment(models.Model):
+    payment_user_id = models.ForeignKey(PaymentUser, on_delete=models.CASCADE)
     penalty_fee_amount = models.DecimalField(max_digits=10, decimal_places=2)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     service_id = models.ForeignKey(Services, on_delete=models.CASCADE, default=1)
